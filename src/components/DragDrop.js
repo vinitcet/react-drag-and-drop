4615c96e800a9a1adca708606a95d6a630da017a
Vinit Kumar <Vinit.Kumar@efi.com>
2022-11-21 16:14:47 +0530

    Initial commit

---
+++ src/components/DragDrop.js
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import Picture from "./Picture";
+import { useDrop } from "react-dnd";
+import "../App.css";
+
+const PictureList = [
+  {
+    id: 1,
+    url:
+      "https://eproductivitysoftware.com/wp-content/uploads/2022/07/EPS-PRINT_PRIMARY-LOGO_SCREEN-300x121.png",
+  },
+];
+
+function DragDrop() {
+  const [board, setBoard] = useState([]);
+
+  const [{ isOver }, drop] = useDrop(() => ({
+    accept: "image",
+    drop: (item) => addImageToBoard(item.id),
+    collect: (monitor) => ({
+      isOver: !!monitor.isOver(),
+    }),
+  }));
+
+  const addImageToBoard = (id) => {
+    const pictureList = PictureList;
+    //const pictureList = PictureList.filter((picture) => id === picture.id);
+    setBoard((board) => [...board, pictureList[0]]);
+  };
+  return (
+    <>
+      <div className="Pictures">
+        {PictureList.map((picture) => {
+          return <Picture url={picture.url} id={picture.id} />;
+        })}
+      </div>
+      <div className="Board" ref={drop}>
+        {board.map((picture) => {
+          return <Picture url={picture.url} id={picture.id} />;
+        })}
+      </div>
+    </>
+  );
+}
+
+export default DragDrop;
